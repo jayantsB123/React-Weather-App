e1985607e91f6faa2a5ecc1f6e5bfd32faa4388c
JAYANT SINGH <97082996+jayantsB123@users.noreply.github.com>
2024-02-06 00:31:13 +0530

    Update Credit.js

--- src/components/Credit.js
+++ src/components/Credit.js
@@ -5,9 +5,9 @@ export default function Credit() {
     <div class="credit">
     <p>
       Coded by
-      <a href="https://www.shecodes.io/graduates/73033-chaimae-lamirine"
-        > Chaimae Lamirine</a
-      >, <a href="https://github.com/shemmee/React-Weather-App">Open sourced on Github.</a> Hosted on Netlify
+      <a href="https://github.com/jayantsB123"
+        >Jayant Singh Bhadauria</a
+      >, <a href="https://github.com/jayantsB123/React-Weather-App">Open sourced on Github.</a> Hosted on Netlify
     </p>
   </div>
   );
